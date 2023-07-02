b611fbf4f8728fc6d166ac9e7a3afc8ac09395a5
CJ Mayes <66642883+CJ-Mayes@users.noreply.github.com>
2023-07-02 15:53:35 +0100

    Update main.py

--- 2023 Blogs/2023-07-06 Create A ChatGPT App For Slack/main.py
+++ 2023 Blogs/2023-07-06 Create A ChatGPT App For Slack/main.py
@@ -7,10 +7,10 @@ from langchain.memory.buffer_window import ConversationBufferWindowMemory
 
 
 openai_api_key = OPENAI_API_TOKEN
-# Initializes your app with your bot token and socket mode handler
+# Start your app with your bot token and socket mode handler
 app = App(token=SLACK_BOT_TOKEN)
 
-# Langchain implementation
+# Langchain setup
 template = """Assistant is a large language model trained by OpenAI.
     {history}
     Human: {human_input}
@@ -28,7 +28,7 @@ chatgpt_chain = LLMChain(
     memory=ConversationBufferWindowMemory(k=2),
 )
 
-# Message handler for Slack
+# Slack handler
 @app.message(".*")
 def message_handler(message, say, logger):
     print(message)
@@ -36,6 +36,6 @@ def message_handler(message, say, logger):
     output = chatgpt_chain.predict(human_input=message['text'])
     say(output)
 
-# Start your app
+# Start the app
 if __name__ == "__main__":
     SocketModeHandler(app, SLACK_APP_TOKEN).start()
